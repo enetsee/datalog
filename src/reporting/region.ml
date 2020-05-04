8c84076ff6e836270afe47e5fa256a1de09c8c6f
Michael Thomas <mthomas180@gmail.com>
2020-05-04 15:48:58 +0100

    Helpers for empty region

--- src/reporting/region.ml
+++ src/reporting/region.ml
@@ -10,6 +10,7 @@ type t =
 let empty = { start_ = Position.empty; end_ = Position.empty }
 let merge { start_; _ } { end_; _ } = { start_; end_ }
 
+let is_empty {start_;end_} = Position.is_empty start_ && Position.is_empty end_
 let merge_many = function
   | [] -> empty
   | init :: rest -> List.fold_left rest ~init ~f:merge
